e570cde44b74e8dbb38a21260200789c9aa7bb42
Lucie <fontaluc@gmail.com>
2021-06-21 11:25:28 +0200

    All changes
    Commit all changes

---
+++ Assets/Plugins/PitchDetector/Demo/Scripts/Graph.cs
@@ -0,0 +1,58 @@
+﻿using FinerGames.PitchDetector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+
+    public LineRenderer lineRenderer;
+    public PitchDetector detector;
+    float curTime;
+    float startTime = 0;
+    private float firstPitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer.startColor = Color.red; //The color at the beginning of the polyline
+        lineRenderer.endColor = Color.red; //The color at the end of discount
+        lineRenderer.startWidth = 3f; //The width at the beginning of the polyline
+        lineRenderer.endWidth = 3f; //The width at the end of the polyline
+        lineRenderer.useWorldSpace = false; //Use world coordinates or local coordinates
+
+    }
+
+
+    // What needs to be noted here is that before using this method, you must ensure that there is at least one straight line in front, that is to say, two points have been set, otherwise
+    // The system will default the position of the first two points to Vector3.zero
+    public void AddPosition(Vector3 v3)
+    {
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1, v3);
+    }
+
+    public void Reset()
+    {
+        lineRenderer.positionCount = 0;
+        startTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (detector.Pitch > 1.0f)
+        {
+            curTime = Time.time;
+            if (startTime == 0)
+            {
+                startTime = curTime;
+                firstPitch = detector.Pitch;
+            }
+            
+            AddPosition(new Vector3((curTime - startTime) * 100, detector.Pitch - firstPitch));
+            Debug.Log(curTime);
+            Debug.Log(detector.Pitch);
+        }
+    }
+}
---
+++ Assets/Plugins/PitchDetector/Demo/Scripts/UIManager.cs
@@ -0,0 +1,93 @@
+﻿using FinerGames.PitchDetector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] MicrophoneInput microInput;
+    [SerializeField] Graph graph;
+    [SerializeField] Text score;
+    [SerializeField] Text buttonText;
+
+    private LineRenderer lineRenderer;
+    private int counter = 0;
+
+    private AudioSource source;
+    private AudioClip recording;
+    float startRecordingTime;
+
+    private void Start()
+    {
+        lineRenderer = graph.GetComponent<LineRenderer>();
+        source = GetComponent<AudioSource>();
+    }
+
+    public void ChangeText()
+    {
+        counter = (counter + 1)%2;
+        if (counter == 1)
+        {
+            buttonText.text = "Arrêter";
+            graph.Reset();
+            score.text = "";
+            microInput.IsRecording = true;
+
+            //Start the recording, the length of 300 gives it a cap of 5 minutes
+            //recording = Microphone.Start("", false, 300, 44100);
+            startRecordingTime = Time.time;
+        }
+        else
+        {
+            buttonText.text = "Démarrer";
+            microInput.IsRecording = false;
+
+            //Microphone.End("");
+
+            recording = microInput.Source.clip;
+            //Trim the audioclip by the length of the recording
+            AudioClip recordingNew = AudioClip.Create(recording.name, (int)((Time.time - startRecordingTime) * recording.frequency), recording.channels, recording.frequency, false);
+            float[] data = new float[(int)((Time.time - startRecordingTime) * recording.frequency)];
+            Debug.Log((int)((Time.time - startRecordingTime) * recording.frequency));
+            recording.GetData(data, 0);
+            recordingNew.SetData(data, 0);
+            this.recording = recordingNew;
+
+            source.clip = recording; 
+
+            // Calculs 
+
+            int numberCount = lineRenderer.positionCount;
+            Vector3[] positions = new Vector3[numberCount];
+            lineRenderer.GetPositions(positions);
+            double[] freq = new double[numberCount];
+            for (int i = 0; i < numberCount; i++)
+            {
+                freq[i] = positions[i].y;
+            }
+
+            // Médiane
+
+            int halfIndex = numberCount / 2;
+            List<double> sortedNumbers = freq.OrderBy(n => n).ToList();
+            double median;
+            if ((numberCount % 2) == 0)
+            {
+                median = (sortedNumbers[halfIndex] + sortedNumbers[halfIndex - 1]) / 2;
+            }
+            else
+            {
+                median = sortedNumbers[halfIndex];
+            }
+            Debug.Log(median);
+            score.text = Math.Round(freq[numberCount - 1] - median, 1).ToString();
+        }
+    }
+
+    public void Replay()
+    {
+        source.Play();
+    }
+}
